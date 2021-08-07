edb3e9b771f996d451f80de11f61fd58b21cb29d
Dourado8083 <gustavo.dourado.br@gmail.com>
2021-08-06 23:22:35 -0300

    Criação do método post de Postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,9 +13,17 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
+tema:Tema = new Tema()
+postagem:Postagem = new Postagem()
+listaTemas: Tema[]
+idTema:number
+user:User = new User()
+idUser = environment.id
   constructor(
-    private router:Router
+    private router:Router,
+    private postageService:PostagemService,
+    private temaService:TemaService
+    /*injetado o service de tema para pegar os temas */
   ) { }
 
   ngOnInit(){
@@ -19,6 +32,28 @@ export class InicioComponent implements OnInit {
     alert('Sua Sessão expirou,faça o login novamente.') */
     this.router.navigate(['/entrar'])
   }
+  this.getAllTemas()
   }
-
+  getAllTemas(){
+  this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+   this.listaTemas = resp
+  })
+  }
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp:Tema)=>{
+    this.tema = resp
+    })
+  }
+publicar(){
+this.tema.id = this.idTema
+this.postagem.tema = this.tema
+/*pegou o usuário certo na this.idUser e depois passou para postagem */
+this.user.id = this.idUser
+this.postagem.usuario = this.user
+this.postageService.postPostagem(this.postagem).subscribe((resp:Postagem)=>{
+this.postagem = resp
+alert('Postagem realizada co sucesso!')
+this.postagem = new Postagem() /*limpeza de campo */
+})
+}
 }
